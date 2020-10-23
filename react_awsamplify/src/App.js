c2c63ca4c90151838f9c21881741dda7bb61027f
felix montanez <felix.montanez@gmail.com>
2020-10-23 15:37:40 -0700

    updated pathways for app update.

--- react_awsamplify/src/App.js
+++ react_awsamplify/src/App.js
@@ -4,18 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>react, aws amplify + ?</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Amplify App</h1>
+      <p>react, graphQL, aws amplify</p>
     </div>
   );
 }
