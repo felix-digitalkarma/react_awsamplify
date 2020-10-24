87c60b221f5c6b1084506314876c043313df79a3
felix montanez <felix.montanez@gmail.com>
2020-10-23 19:19:43 -0700

    updated to include graphql from src folder, rather than front end folder.

--- react_awsamplify/src/hooks/landing.js
+++ react_awsamplify/src/hooks/landing.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import createTodo from "../../../src/graphql/mutations";
+import listTodos from "../../../src/graphql/queries.gql";
+import awsExports from "../../../src/aws-exports";
+
 import { Helmet } from "react-helmet-async";
 import styled from "styled-components";
 import Avatar from "../components/Avatar";
@@ -6,12 +12,41 @@ import python_logo from "../assets/python_logo.png";
 import circuit_logo from "../assets/circuit.png";
 import ai_logo from "../assets/ai_logo.png";
 
+Amplify.configure(awsExports);
+
 const ImageIcons = styled(Avatar)`
   margin-left: 10px;
   margin-right: 10px;
 `;
 
-// Something here
+const styles = {
+  container: {
+    width: 400,
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: "none",
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: "black",
+    color: "white",
+    outline: "none",
+    fontSize: 18,
+    padding: "12px 0px",
+  },
+};
+
 const ImageContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -22,7 +57,41 @@ const ImageContainer = styled.div`
   }
 `;
 
+const initialState = { name: "", description: "" };
+
 const Landing = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  }
   return (
     <div className="text-center cover-container d-flex p-3 mx-auto flex-column">
       <Helmet>
@@ -61,6 +130,28 @@ const Landing = () => {
           service, sponsorship, and support for our local and global
           communities.
         </p>
+        <h2>Amplify Todos</h2>
+        <input
+          onChange={(event) => setInput("name", event.target.value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <input
+          onChange={(event) => setInput("description", event.target.value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <button style={styles.button} onClick={addTodo}>
+          Create Todo
+        </button>
+        {todos.map((todo, index) => (
+          <div key={todo.id ? todo.id : index} style={styles.todo}>
+            <p style={styles.todoName}>{todo.name}</p>
+            <p style={styles.todoDescription}>{todo.description}</p>
+          </div>
+        ))}
       </main>
     </div>
   );
